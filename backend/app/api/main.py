da966892698fdd4da956b15fd8537385afc64b1c
Yongqiang Liu <darmenliu@gmail.com>
2024-11-17 17:22:28 +0800

    Add empty training data api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils, finetune
+from app.api.routes import items, login, users, utils, finetune, training_data
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -8,3 +8,4 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(finetune.router, prefix="/finetune", tags=["finetune"])
+api_router.include_router(training_data.router, prefix="/training-data", tags=["training-data"])
